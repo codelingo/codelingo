32c3c29446f3c2597a6ef06b7b7ca745bd3338fb
codelingo <hello@codelingo.io>
2018-08-01 10:46:57 +1200

    Test review comment

--- tenets/codelingo/go/sprintf/example.go
+++ tenets/codelingo/go/sprintf/example.go
@@ -14,6 +14,10 @@ func doStuff(param string) error {
 	return errors.New(fmt.Sprintf("Don't call with \"%s\" param - it will literally do nothing!", param))
 }
 
-func doStuff(param string) error {
+func paramErr(param string) error {
+	return errors.New(fmt.Sprintf("parameter error: \"%s\"", param))
+}
+
+func doOtherStuff(param string) error {
 	return errors.Errorf("Don't call with \"%s\" param - it will literally do nothing!", param)
 }
