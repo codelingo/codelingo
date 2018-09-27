bd90b104358bf1da589e2ce9372fb10e71ccfadb
Shane Mulligan <mullikine@gmail.com>
2018-09-27 13:57:08 +1200

    removed misleading comments from example.go

--- tenets/codelingo/go/global-var/example.go
+++ tenets/codelingo/go/global-var/example.go
@@ -31,28 +31,21 @@ var myVar1, myVar2 = 1, 2
 
 var _ = 0
 
-// Those are not errors
 var myVar = 1
 
-// Those are fake errors which are currently not detected
-// because they start with 'err' or 'Err' and we don't
-// check if such a variable implements the error interface.
 var errFakeErrorUnexported = 1
 var ErrFakeErrorExported = 1
 
-// Those errors are not named correctly
 var myErrVar = errors.New("myErrVar")
 var myVarErr = errors.New("myVarErr")
 var myVarError = errors.New("myVarErr")
 var customErr = customError{"customErr"}
 
-// Those are actual errors which should be ignored
 var errUnexported = errors.New("errUnexported")
 var ErrExported = errors.New("ErrExported")
 var errCustomUnexported = customError{"errCustomUnexported"}
 var ErrCustomExported = customError{"ErrCustomExported"}
 
-// Those actual errors have a declared error type
 var declaredErr error = errors.New("declaredErr")
 var errDeclared error = errors.New("errDeclared")
 
@@ -60,29 +53,24 @@ type customError struct{ e string }
 
 func (e *customError) Error() string { return e.e }
 
+// These should also be detected as global variables
 var (
 	// Those are not errors
 	myVar = 1
 
-	// Those are fake errors which are currently not detected
-	// because they start with 'err' or 'Err' and we don't
-	// check if such a variable implements the error interface.
 	errFakeErrorUnexported = 1
 	ErrFakeErrorExported   = 1
 
-	// Those errors are not named correctly
 	myErrVar   = errors.New("myErrVar")
 	myVarErr   = errors.New("myVarErr")
 	myVarError = errors.New("myVarErr")
 	customErr  = customError{"customErr"}
 
-	// Those are actual errors which should be ignored
 	errUnexported       = errors.New("errUnexported")
 	ErrExported         = errors.New("ErrExported")
 	errCustomUnexported = customError{"errCustomUnexported"}
 	ErrCustomExported   = customError{"ErrCustomExported"}
 
-	// Those actual errors have a declared error type
 	declaredErr error = errors.New("declaredErr")
 	errDeclared error = errors.New("errDeclared")
 )
