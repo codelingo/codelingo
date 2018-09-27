16cf9005e3498b8595a40d77c5ba2657301c999c
Shane Mulligan <mullikine@gmail.com>
2018-09-27 14:18:44 +1200

    noinit tenet

---
+++ tenets/codelingo/go/init/example.go
@@ -0,0 +1,33 @@
+package code
+
+var myVar = 0
+
+type S struct{}
+
+func (s S) init(arg string) bool {
+	yourVar := true
+	return yourVar
+}
+
+var theVar = true
+
+type S struct{}
+
+const constant = 0
+
+func function(arg string) bool {
+	yourVar := true
+	return yourVar
+}
+
+func init() {
+}
+
+type S struct{}
+
+func function() {
+	init := func() {}
+	init()
+}
+
+var _ = 0
