48027ed746e5b1d9d27f073af899e25798745b59
Emerson Wood <emersonwood94@gmail.com>
2018-09-28 16:44:15 +1200

    Add println format strings tenet (#76)

---
+++ tenets/codelingo/go/println-format-strings/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	correct()
+	incorrect()
+}
+
+func correct() {
+	fmt.Println("hello world")
+	fmt.Printf("hello %s\n", "world")
+
+	var str string
+	str = fmt.Sprintln("hello %s", "world")
+	fmt.Print(str)
+	str = fmt.Sprintf("4 + 6 = %d\n", 4+6)
+	fmt.Print(str)
+}
+
+func incorrect() {
+	fmt.Println("hello %s", "world") // ISSUE
+
+	var str string
+	str = fmt.Sprint("hello %s\n", "world") // ISSUE
+	fmt.Print(str)
+	str = fmt.Sprint("4 + 6 = %d\n", 4+6) // ISSUE
+	fmt.Print(str)
+}
