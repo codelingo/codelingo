7f701517f5ff73ef3fb2f57296451aecf0e84d39
codelingo <hello@codelingo.io>
2018-10-17 12:05:07 +1300

    remove bins, add cross build script

--- sdk/flow/buildall.sh
+++ sdk/flow/buildall.sh
@@ -10,10 +10,55 @@ fi
 
 
 codelingoPath="$path/src/github.com/codelingo/codelingo"
+v="
+windows,386;\
+linux,386;\
+windows,amd64;\
+linux,amd64;\
+darwin,amd64;"
+
+# TODO(waigani) cmds should define versions
+version=0.0.0
 
 for owner in $codelingoPath/flows/*/ ; do
 	for d in $owner*/ ; do
+
 	    cd $d
-	    go build -o ./cmd
+
+		# Build and push each bin to release
+		echo $v | while IFS=',' read -d';' os arch;  do 
+
+			cd $d
+
+		    if [ "$os" == "windows" ]; then
+		    ext=.exe
+		    else
+		    ext=""
+		    fi
+
+		    binpath=./bin/$os/$arch/$version
+	    	mkdir -p $binpath
+			GOOS=$os GOARCH=$arch go build -o $binpath/cmd$ext
+
+			cd $binpath
+			filename=cmd$ext
+			if [ "$os" == "windows" ]; then
+				fn="$filename.zip"
+		        rm $binpath/$fn
+				zip $fn cmd.exe
+		        rm cmd.exe
+			else
+				fn="$filename.tar.gz"
+				tar -cvzf $fn cmd
+			    rm cmd
+		    fi
+		    cd ..
+
+		done
+
 	done
 done
+
+
+
+echo "Done!"
