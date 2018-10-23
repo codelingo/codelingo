eb9668a223b0e240d832e1cc13addd728755243e
Shane Mulligan <mullikine@gmail.com>
2018-10-24 11:43:41 +1300

    shadowed func para

--- tenets/codelingo/k8/shadowed-func-parameter/example.go
+++ tenets/codelingo/k8/shadowed-func-parameter/example.go
@@ -1,11 +1,26 @@
 package main
 
-// Heuristic:
-// ‾‾‾‾‾‾‾‾‾‾
-// e.g. result, err := tryTheThing(). look for err := within a function where err is a parameter
-
 import "fmt"
+import "errors"
+
+func tryTheThing() (string, error) {
+	return "hi", nil
+}
+
+var ErrDidNotWork = errors.New("did not work")
+
+func DoTheThing(reallyDoIt bool) (err error) {
+  if reallyDoIt {
+    result, err := tryTheThing()
+    if err != nil || result != "it worked" {
+      err = ErrDidNotWork
+    }
+  }
+  return err
+}
 
 func main() {
-    :
+	e := DoTheThing(true)
+	fmt.Printf("%v\n", e)
+	// e.g. result, err := tryTheThing(). look for err := within a function where err is a parameter
 }
