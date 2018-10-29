c2141a29bf91b6813d5230508a5f1e3645543f16
Shane Mulligan <mullikine@gmail.com>
2018-10-30 12:58:44 +1300

    polish off 3 tenets

--- tenets/codelingo/k8/well-named-lock/example.go
+++ tenets/codelingo/k8/well-named-lock/example.go
@@ -1,17 +1,16 @@
 package main
 
-// Heuristic:
-// ‾‾‾‾‾‾‾‾‾‾
-// When there is only ONE lock inside the same scope, ensure its name is 'lock'.
-
 import "fmt"
 
 type anyStruct struct {
-        lock sync.Mutex
+	lock sync.Mutex
 }
 
-func afunc(c *anyStruct) {
-        c.lock.Lock()
-        defer c.lock.Unlock()
+type badStruct struct {
+	badLock sync.Mutex
 }
 
+type MockAddresses struct {
+	lockA sync.Mutex
+	lockB sync.Mutex
+}
