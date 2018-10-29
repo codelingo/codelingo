6907e33b714f92296dcd8b3cafdc01f7262c0785
Shane Mulligan <mullikine@gmail.com>
2018-10-30 10:03:39 +1300

    k8 tents WIP

--- tenets/codelingo/k8/well-named-lock/example.go
+++ tenets/codelingo/k8/well-named-lock/example.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
-
-func main() {
-    :
-}
-
 // Heuristic:
 // ‾‾‾‾‾‾‾‾‾‾
 // When there is only ONE lock inside the same scope, ensure its name is 'lock'.
+
+import "fmt"
+
+type anyStruct struct {
+        lock sync.Mutex
+}
+
+func afunc(c *anyStruct) {
+        c.lock.Lock()
+        defer c.lock.Unlock()
+}
+
