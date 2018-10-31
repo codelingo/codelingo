c02f11eb06eac45e02d28eac36ed017aefec6be8
rstorr <rstorr95@gmail.com>
2018-11-01 12:50:58 +1300

    tenet ignores const's when looking for globals.

--- tenets/codelingo/go/global-var/example.go
+++ tenets/codelingo/go/global-var/example.go
@@ -1,35 +1,21 @@
-package code
-
-import (
-	"errors"
-)
+package main
 
 const c1 = 0
 
-const (
-	c2 = 1
-	c3 = 2
-)
-
 var v1 = 0
-var v2, v3 = 1, 2
-var _ = 0
 
-func someCode() bool {
-	v4 := true
-	return v4
-}
-
-var errOne = errors.New("myErrVar")
-
-var errTwo error = errors.New("declaredErr")
-
-type customError struct{ e string }
-
-func (e *customError) Error() string { return e.e }
-
-// These should also be detected as global variables
 var (
-	v5                = 1
-	errDeclared error = errors.New("errDeclared")
+  v2 = 1
+  v3 = 2
 )
+
+const (
+  c2 = 1
+  c3 = 2
+)
+
+
+func main() {
+  var v4 = 3
+  const c4 = 3
+}
