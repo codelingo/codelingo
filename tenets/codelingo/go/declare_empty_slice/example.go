e11c0be8d9e95f331b001ec94a1ec3e9c68d52cf
rstorr <rstorr95@gmail.com>
2018-11-01 14:55:19 +1300

    Declare empty slice tenet for golang project.

---
+++ tenets/codelingo/go/declare_empty_slice/example.go
@@ -0,0 +1,18 @@
+package main
+
+var t []string
+
+func main() {
+
+  var t []string
+  t := []string{}
+  t := [2]string{2,3}
+  
+  i := 0
+  for i < 10 {
+    var w []string
+    w := []string{}
+    w := [2]string{2,3}
+    i++
+  }
+}
