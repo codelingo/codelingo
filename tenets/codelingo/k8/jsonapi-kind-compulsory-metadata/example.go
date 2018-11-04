704379d5e711991507d9010eb81f03dccdfa988e
Shane Mulligan <mullikine@gmail.com>
2018-11-05 11:53:54 +1300

    jsonapi-kind-compulsory-metadata

--- tenets/codelingo/k8/jsonapi-kind-compulsory-metadata/example.go
+++ tenets/codelingo/k8/jsonapi-kind-compulsory-metadata/example.go
@@ -1,11 +1,17 @@
 package main
 
-// Heuristic:
-// ‾‾‾‾‾‾‾‾‾‾
-// https://mullikine.github.io/codelingo/jsonapi-metadata.html
-
 import "fmt"
 
-func main() {
-    :
+// Is this a kind or an object?
+
+type NetworkPolicy struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// Specification of the desired behavior for this NetworkPolicy.
+	// +optional
+	Spec NetworkPolicySpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
 }
