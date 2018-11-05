603434b0e51ee9979292a042cb9e228d8afc05e9
Shane Mulligan <mullikine@gmail.com>
2018-11-05 13:41:19 +1300

    added comment to example.go

--- tenets/codelingo/k8/jsonapi-compulsory-fields/example.go
+++ tenets/codelingo/k8/jsonapi-compulsory-fields/example.go
@@ -2,6 +2,7 @@ package main
 
 // Heuristic:
 // ‾‾‾‾‾‾‾‾‾‾
+// this used to live inside of staging. but i have removed that when testing kubernetes
 // go files deep inside the 'k8s.io/api/' folder. find .go files that import metav1 k8s.io/apimachinery/pkg/apis/meta/v1 and define a bunch of structs. detecting import and struct may not be necessary. We want to find files that do not have kind and apiVersion
 
 import "fmt"
