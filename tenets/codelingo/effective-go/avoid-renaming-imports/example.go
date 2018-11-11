169f04183b9a39f84080ae1c7c1b88363e62ac52
daanikus <daanikus@gmail.com>
2018-11-12 11:27:02 +1300

    Added a few Effective Go tenets

---
+++ tenets/codelingo/effective-go/avoid-renaming-imports/example.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	math "math/rand"
+	_ "os"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+}
---
+++ tenets/codelingo/effective-go/underscores-in-name/example.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	var a int = 15
+	a_bad_name := 20
+	var another_bad_name string = "hello"
+	aGoodVar := 2
+}
+
+func aFunction() {
+	fmt.Println("This name is fine")
+}
+
+func a_bad_function() {
+	fmt.Println("Needs a name change")
+}
+
+type Bad_Interface interface {
+	SomeMethod() int
+}
+
+type BetterInterface interface {
+	SomeMethod() int
+}
