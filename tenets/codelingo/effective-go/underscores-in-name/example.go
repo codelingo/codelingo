966e4638021e1be0d0b7b656fe4fd21855a833c8
daanikus <daanikus@gmail.com>
2018-11-13 12:28:41 +1300

    Make fixes to example go file based on review flow comments

--- tenets/codelingo/effective-go/underscores-in-name/example.go
+++ tenets/codelingo/effective-go/underscores-in-name/example.go
@@ -2,9 +2,9 @@
 package main
 
 func main() {
-	var a int = 15
+	var a = 15
 	a_bad_name := 20
-	var another_bad_name string = "hello"
+	var another_bad_name = "hello"
 	aGoodVar := 2
 }
 
@@ -16,10 +16,11 @@ func a_bad_function() {
 	fmt.Println("Needs a name change")
 }
 
+// Bad_Interface used as example
 type Bad_Interface interface {
 	SomeMethod() int
 }
-
+// BetterInterface used as example
 type BetterInterface interface {
 	SomeMethod() int
 }
