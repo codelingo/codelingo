188d0798f024beb49062d338a7a3ce9811320e5b
daanikus <daanikus@gmail.com>
2018-11-13 09:43:44 +1300

    Added EG tenet to check for annotations in comments

---
+++ tenets/codelingo/effective-go/avoid-annotations-in-comments/example.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//This is a comment
+	fmt.Println("Hello, playground")
+	/* This is another comment _this_
+	 *
+	 */
+	fmt.Println("Hello")
+	// This comment is fine and should not be picked up
+	fmt.Println("Hello")
+	// <this> tag should be caught
+}
