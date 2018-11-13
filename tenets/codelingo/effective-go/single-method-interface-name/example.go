e5b76ad408dbf493160d9e69a9f50ce481c1c122
daanikus <daanikus@gmail.com>
2018-11-14 11:36:49 +1300

    Added interface comments to please the review bot

--- tenets/codelingo/effective-go/single-method-interface-name/example.go
+++ tenets/codelingo/effective-go/single-method-interface-name/example.go
@@ -5,27 +5,33 @@ import (
 	"fmt"
 )
 
+// Reader is an example interface
 type Reader interface {
 	Read() []byte
 }
 
+// SomeName is an example interface
 type SomeName interface {
 	Read() []byte
 }
 
+// Writer is an example interface
 type Writer interface {
 	Write() int
 }
 
+// SomeWriter is an example interface
 type SomeWriter interface {
 	Write() int
 }
 
+// DoesntCount is an example interface
 type DoesntCount interface {
 	Write() int
 	Read() []byte
 }
 
+// Read is an example interface
 type Read interface {
 	Reader() []byte
 }
