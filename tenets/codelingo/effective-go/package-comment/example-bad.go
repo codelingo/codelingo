1c9352835bcdc5b4559520021f98213abd79d2b9
daanikus <daanikus@gmail.com>
2018-11-16 16:20:10 +1300

    Added EG tenet to check for package comments

---
+++ tenets/codelingo/effective-go/package-comment/example-bad.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+)
+
+func Thing() {
+    fmt.Println("This is a func")
+}
---
+++ tenets/codelingo/effective-go/package-comment/example-good.go
@@ -0,0 +1,11 @@
+/* This package is okay as it contains a package comment
+*/
+package main
+
+import (
+    "fmt"
+)
+
+func Thing() {
+    fmt.Println("This is a func")
+}
