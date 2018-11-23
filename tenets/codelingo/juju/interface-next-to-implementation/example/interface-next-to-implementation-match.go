02ea81e11c26772c8645f7ec00695e362ce3fd85
Shane Mulligan <mullikine@gmail.com>
2018-11-23 20:15:03 +1300

    interface-next-to-implementation

---
+++ tenets/codelingo/juju/interface-next-to-implementation/example/interface-next-to-implementation-match.go
@@ -0,0 +1,14 @@
+package mypack
+
+type geometry interface {
+    area() float64
+    perim() float64
+}
+
+func (c circle) area() float64 {
+    return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+    return 2 * math.Pi * c.radius
+}
