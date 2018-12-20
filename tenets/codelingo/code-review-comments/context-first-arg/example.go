56dee1c9bf102b1697e7b40244b3087a275c8306
daanikus <daanikus@gmail.com>
2018-12-20 15:57:26 +1300

    go fmt example file

--- tenets/codelingo/code-review-comments/context-first-arg/example.go
+++ tenets/codelingo/code-review-comments/context-first-arg/example.go
@@ -8,18 +8,17 @@ func F(ctx context.Context, a int)               {}
 func G(b int, ctx context.Context, a int)        {}
 func H(c int, b int, ctx context.Context, a int) {}
 
-
 // Don't catch them in structs
 type IsTypeOfParams struct {
-    // Value that needs to be resolve.
-    // Use this to decide which GraphQLObject this value maps to.
-    Value interface{}
+	// Value that needs to be resolve.
+	// Use this to decide which GraphQLObject this value maps to.
+	Value interface{}
 
-    // Info is a collection of information about the current execution state.
-    Info ResolveInfo
+	// Info is a collection of information about the current execution state.
+	Info ResolveInfo
 
-    // Context argument is a context value that is provided to every resolve function within an execution.
-    // It is commonly
-    // used to represent an authenticated user, or request-specific caches.
-    Context context.Context
+	// Context argument is a context value that is provided to every resolve function within an execution.
+	// It is commonly
+	// used to represent an authenticated user, or request-specific caches.
+	Context context.Context
 }
