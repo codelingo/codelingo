d3533b04d8a076fc2f18c8b57e683defa51b82e8
BlakeMScurr <blakemscurr@gmail.com>
2019-03-08 16:31:42 +1300

    Add test cases for error discarding.

--- tenets/codelingo/code-review-comments/do-not-discard-errors/example.go
+++ tenets/codelingo/code-review-comments/do-not-discard-errors/example.go
@@ -8,11 +8,16 @@ import (
 
 func main() {
 	fmt.Println("Hello, playground")
-	a, _ := example()
+	a, _ := example() // ISSUE
 	fmt.Println(a)
 	fmt.Println(err)
 }
 
 func example() (int, error) {
 	return 1, errors.New("some error")
+}
+
+func trickyReturnExample() (int, *string, string, bool, error) {
+	i, _ := example() // ISSUE
+	return i, nil, "hello", true, nil
 }
