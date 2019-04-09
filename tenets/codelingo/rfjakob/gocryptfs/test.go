23652b3cad2ce5208fee6f7bbc51b14b323cbc3b
leilaes <leila.eskandari@hotmail.com>
2019-04-09 14:41:54 +1200

    Add example and json files

---
+++ tenets/codelingo/rfjakob/gocryptfs/test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	f, err := os.Open("/home/leila/test.txt")
+	check(err)
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1))
+	//f.Close()
+
+}
