396c223b56c939a1592d591c18ae988f38e26170
Dan Bent <daanikus@gmail.com>
2019-04-12 11:43:09 +1200

    Add tenet to check receivers of structs with mutex

---
+++ tenets/codelingo/code-review-comments/structs-with-mutex/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "sync"
+
+type thing struct {
+	name  string
+	num   int
+	mutex sync.Mutex
+}
+
+func (t thing) GetName() string {
+	return t.name
+}
+
+func (t *thing) GetNum() int {
+	return t.num
+}
+
+func main() {}
