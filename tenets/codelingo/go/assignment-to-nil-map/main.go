f440bf2f56ea93ed679574086dc7b7960b12b203
Dan Bent <daanikus@gmail.com>
2019-04-23 12:06:10 +1200

    Add comment to example file

--- tenets/codelingo/go/assignment-to-nil-map/main.go
+++ tenets/codelingo/go/assignment-to-nil-map/main.go
@@ -6,8 +6,7 @@ func main() {
 	now := time.Now().Unix()
 	metrics := []string{"example"}
 
-	var accessTimes map[string]int64
-	// accessTimes = make(map[string]int64)
+	var accessTimes map[string]int64 // ISSUE
 	for _, m := range metrics {
 		accessTimes[m] = now
 	}
