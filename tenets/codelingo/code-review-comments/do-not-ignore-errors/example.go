5f01160f8209a2235a52973f6dc761fc13005601
BlakeMScurr <blakemscurr@gmail.com>
2019-04-30 17:38:54 +1200

    Add rewrite to do-not-ignore-errors.
    Split it out from do-not-discard-errors.

---
+++ tenets/codelingo/code-review-comments/do-not-ignore-errors/example.go
@@ -0,0 +1,48 @@
+//Package main is an example package
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	a, _ := example()
+	fmt.Println(a)
+	b, _ := one()
+	fmt.Println(b)
+
+	example() // ISSUE
+	if true {
+		trickyReturnExample()
+	}
+}
+
+func passing() error {
+	_, err := example()
+	if err != nil {
+		return err
+	}
+
+	i, err := example()
+	if err != nil {
+		return err
+	}
+
+	_ = i
+	return nil
+}
+
+func example() (int, error) {
+	return 1, errors.New("some error")
+}
+
+func one() (int, int) {
+	return 0, 1
+}
+
+func trickyReturnExample() (int, *string, string, bool, error) {
+	i, _ := example() // ISSUE
+	return i, nil, "hello", true, nil
+}
