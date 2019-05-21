496f7fe94759a2de4e275af3fc166bd8eff8e9cf
leilaes <leila.eskandari@hotmail.com>
2019-05-22 09:49:51 +1200

    Update missing close file tenet for rfjakob-gocryptfs

--- tenets/codelingo/rfjakob-gocryptfs/missing-close-file/test.go
+++ tenets/codelingo/rfjakob-gocryptfs/missing-close-file/test.go
@@ -10,15 +10,60 @@ func check(e error) {
 		panic(e)
 	}
 }
+func test1() *os.File {
+	f, err := os.Open("/tmp/test.txt")
+	check(err)
+	return f
+}
+
+func test2() {
+	f, err := os.Open("/tmp/test.txt") //ISSUE
+	check(err)
+	b := make([]byte, 5)
+	n, err := f.Read(b)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n, string(b))
+	//f.Close()
+}
+
+func test3() (f *os.File) {
+	f, err := os.Open("/tmp/test.txt")
+	check(err)
+	return
+}
 
 func main() {
 
-	f, err := os.Open("/tmp/test.txt")
+	f1, err := os.Open("/tmp/test.txt") //ISSUE
 	check(err)
 	b1 := make([]byte, 5)
-	n1, err := f.Read(b1)
+	n1, err := f1.Read(b1)
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
-	//f.Close()
+	//f1.Close()
+
+	f2, err := os.Open("/tmp/test.txt")
+	check(err)
+	b2 := make([]byte, 5)
+	n2, err := f2.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n2, string(b2))
+	f2.Close()
+
+	f3, err := os.OpenFile("/tmp/test.txt", os.O_RDWR, 0644) //ISSUE
+	check(err)
+	b3 := make([]byte, 5)
+	n3, err := f3.Read(b3)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n3, string(b3))
+	//f3.Close()
+
+	f4, err := os.Open("/tmp/test.txt")
+	check(err)
+	defer f4.Close()
+	b4 := make([]byte, 5)
+	n4, err := f4.Read(b4)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n4, string(b4))
 
 }
