44e41e200658f43d56bd76cb0e1df5041b571773
CallanTaylor <taylorcallan96@gmail.com>
2019-10-09 15:27:25 +1300

    Fixed tenet and added comment describing issue

--- tenets/codelingo/go/shared-count-datarace/example.go
+++ tenets/codelingo/go/shared-count-datarace/example.go
@@ -6,11 +6,19 @@ import (
 
 func main() {
 
+	// Issue - Each goroutine inside this for loop is intended to have a unique
+	// value of i. However this may not always be the case so the goroutine should
+	// be given a copy of I as an argument as in the second for loop.
 	for i := 0; i < 5; i++  {
 		go func() {
-			go func(i int)  {
-				fmt.Println(i)
-			}(i)
+			fmt.Println(i)
 		}()
+	}
+
+	// Non Issue
+	for i := 0; i < 5; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
 	}
 }
